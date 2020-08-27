bef805754276bf41a78808f8d62360ca73d9e411
kedma <kedma06@gmail.com>
2020-08-27 13:48:29 -0300

    primeiroNlW

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+//JSX: sintaxe de XML dentro do JavaScript
+import Routes from './routes';
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
